cb22ea2aece9972f621c0d9e77d961180bb225e3
Kateryna Dokuchaieva <kathareia@gmail.com>
2019-02-10 19:22:21 +0200

    Task 5 bubble-sorting.php

---
+++ bubble-sorting.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = [3, 9, 16, -5, 23, 48, 38];
+
+echo 'Before sorting: ' . PHP_EOL;
+echo json_encode($a) . PHP_EOL;
+
+function bubble(&$a)
+{
+    $l = count($a);
+    for ($i = 0; $i <= $l - 1; $i++) {
+        for ($j = $l; $j >= $i + 1; $j--) {
+            if ($a[$j] < $a[$j - 1]) {
+                $temp = $a[$j];
+                $a[$j] = $a[$j - 1];
+                $a[$j - 1] = $temp;
+            }
+        }
+    }
+}
+
+bubble($a);
+echo 'After sorting: ' . PHP_EOL;
+echo json_encode($a); //I don't understand why there's 'null' at the beginnign of the list :(
