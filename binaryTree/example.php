cad26c04a434b73fcc041bb5be6af3dfa1359664
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-02-26 23:22:50 +0200

    Create example.php

---
+++ binaryTree/example.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__."./tree.php";
+require_once __DIR__."./treeNode.php";
+
+$obj = new binaryTree();
+$obj->insert(18);
+$obj->insert(3);
+$obj->insert(19);
+$obj->insert(77);
+$obj->insert(1);
+$obj->insert(2);
+$obj->insert(20);
+$obj->insert(5);
+$obj->deepestNode();
