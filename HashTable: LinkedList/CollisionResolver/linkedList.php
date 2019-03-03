2ae9d3f65b532585263c6eb4bcbd98e32488e643
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-03-03 19:12:42 +0200

    linkedList.php

---
+++ HashTable: LinkedList/CollisionResolver/linkedList.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . "/Node.php";
+
+class linkedList extends Node
+{
+    private $head;
+    private $tail;
+    private $count = 0;
+
+    public function insert($value)
+    {
+        $newElement = new Node();
+        $newElement->setValue($value);
+
+        if(!empty($this->head)) {
+            $this->tail->setNext($newElement);
+            $this->tail = $newElement;
+
+        } else {
+            $this->head = $newElement;
+            $this->tail = $newElement;
+        }
+
+        $this->count++;
+        $newElement->setPosition($this->count);
+    }
+
+    public function search($value)
+    {
+        $current = $this->head;
+        while(!($value == $current->getValue())) {
+            $current = $current->getNext();
+        }
+        return $current->getPosition();
+    }
+}
