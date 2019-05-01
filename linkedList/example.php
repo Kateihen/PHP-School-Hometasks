96aed6bca5dc08ab140db91b15bc6552c545a6ca
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-05-01 22:39:52 +0300

    Update example.php

--- linkedList/example.php
+++ linkedList/example.php
@@ -4,11 +4,14 @@ require_once __DIR__ . "/linkedList.php";
 
 $linkedList = new linkedList();
 
-$linkedList->append("1hello");
-$linkedList->append("2hello");
-$linkedList->append("3hello");
-$linkedList->prepend("0hello");
+$linkedList->append('1hello');
+$linkedList->append('2hello');
+$linkedList->append('3hello');
+$linkedList->prepend('0hello');
 $linkedList->deleteFromEnd();
 $linkedList->deleteFromHead();
-$linkedList->search("2hello");
-$linkedList->delete();
+$res = $linkedList->search("2hello");
+var_dump($res);
+$linkedList->delete('2hello');
+$res1 = $linkedList->search("1hello");
+var_dump($res1);
