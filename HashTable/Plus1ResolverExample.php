888b47f0d11a72ae5e0121341abd52735947d2ce
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-05-01 19:59:39 +0300

    Create Plus1ResolverExample.php

---
+++ HashTable/Plus1ResolverExample.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . "/CollisionResolver/ResolverInterface.php";
+require_once __DIR__ . "/hashTable.php";
+require_once __DIR__ . "/hashFunction.php";
+require_once __DIR__ . "/CollisionResolver/CollisionResolverLinkedList.php";
+require_once __DIR__ . "/CollisionResolver/Node.php";
+require_once __DIR__ . "/CollisionResolver/linkedList.php";
+require_once __DIR__ . "/CollisionResolver/collisionResolverPlus1.php";
+
+$hashTableLenght = 12;
+$hashTable = new HashTable($hashTableLenght, new CollisionResolverPlus1());
+
+$str1 = "Ada";
+$str2 = "Max";
+$str3 = "Aad";
+
+$hashFunction1 = new HashFunction($str1, $hashTableLenght);
+$hashTable->write($hashFunction1(), $str1);
+
+$hashFunction2 = new hashFunction($str2, $hashTableLenght);
+$hashTable->write($hashFunction2(), $str2);
+
+$hashFunction3 = new hashFunction($str3, $hashTableLenght);
+$hashTable->write($hashFunction3(), $str3);
+
+$hashTable->read($hashFunction1(), $str1);
+$hashTable->read($hashFunction2(), $str2);
+$hashTable->read($hashFunction3(), $str3);
