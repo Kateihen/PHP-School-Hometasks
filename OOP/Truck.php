646ddd07064a1621d5d4b7848462d47c9902393c
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-05-02 14:27:36 +0300

    Create Truck.php

---
+++ OOP/Truck.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . "/Car.php";
+
+class Truck extends Car
+{
+	protected $type = 'Truck';
+	private $loadCapacity;
+
+	public function setLoadCapacity(int $c)
+	{
+		if ($c > 9000 || $c < 60000) {
+			$this->loadCapacity = $c;
+		} else {
+			throw new Exception("Load Capacity is wrong.");
+		}
+	}
+
+	public function getInfo() : array
+    {
+        $info = [
+            'Type' => $this->type,
+            'Brand' => $this->brand,
+            'Model' => $this->model,
+            'Year' => $this->year,
+            'VIN-Code' => $this->vinCode,
+            'Load Capacity' => $this->loadCapacity,
+        ];
+        return $info;
+    }
+}
