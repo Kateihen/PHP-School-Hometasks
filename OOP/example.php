581d6b009b72b69b5ac0176380f7e7448c680842
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-05-02 14:29:18 +0300

    Create example.php

---
+++ OOP/example.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . "/Car.php";
+require_once __DIR__ . "/PassengerCar.php";
+require_once __DIR__ . "/Truck.php";
+require_once __DIR__ . "/Catalog.php";
+
+$carreraGT = new PassengerCar();
+
+$carreraGT->setBrand('Porsche');
+$carreraGT->setModel('Carrera GT');
+$carreraGT->setYear(2006);
+$carreraGT->setVinCode('1HGBH41JXMN109186');
+$carreraGT->setPackage('Premium');
+
+$columbia = new Truck();
+
+$columbia->setBrand('Freightline');
+$columbia->setModel('CL Columbia');
+$columbia->setYear(2010);
+$columbia->setVinCode('3HIG4BY4OU2BO8974');
+$columbia->setLoadCapacity(45000);
+
+$cat = new Catalog();
+$cat->addCar($carreraGT);
+$cat->addCar($columbia);
+print_r($cat->getAllCarsInfo());
+echo json_encode($cat->getCarInfo($columbia));
