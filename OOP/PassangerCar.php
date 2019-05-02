999e54dac04f53506e929802b3d8dcec351d336b
Kateihen <46999482+Kateihen@users.noreply.github.com>
2019-05-02 14:26:51 +0300

    Create PassangerCar.php

---
+++ OOP/PassangerCar.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . "/Car.php";
+
+class PassengerCar extends Car
+{
+    protected $type = 'Passenger car';
+    private $package;
+
+    public function setPackage(string $p)
+    {
+        $this->package = $p;
+    }
+
+    public function getInfo() : array
+    {
+        $info = [
+            'Type' => $this->type,
+            'Brand' => $this->brand,
+            'Model' => $this->model,
+            'Year' => $this->year,
+            'VIN-Code' => $this->vinCode,
+            'Package' => $this->package,
+        ];
+        return $info;
+    }
+}
